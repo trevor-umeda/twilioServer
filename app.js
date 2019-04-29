1411064550d6953b240e6f9ab3a0608ffb3eeb05
EC2 Default User <ec2-user@ip-172-31-0-247.us-west-2.compute.internal>
2019-04-29 07:40:11 +0000

    something

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.post('/sms', (req, res) => {
   // const twiml = new MessagingResponse();
 
   // twiml.message('The Robots are coming! Head for the hills!');
-console.log(res.body);
+console.log(req.body.Body);
   // res.writeHead(200, {'Content-Type': 'text/xml'});
   // res.end(twiml.toString());
 });
