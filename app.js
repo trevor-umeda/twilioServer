18d889674ce5fd6297f8d5379360763aaa01ce4a
Trevor Umeda <tumeda@riotgames.com>
2019-08-23 20:07:24 -0700

    Trying better return

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+// app.set('view engine', 'html');
 
 app.get('/', (req, res) => res.send('Hello World!'))
 console.log("Startingu p");
@@ -21,8 +22,11 @@ app.post('/sms', (req, res) => {
   console.log(req.body.Body);
   fs.appendFileSync('incomingMessages', req.body.Body+'\r\n');
   //
-  res.writeHead(200, {'Content-Type': 'text/xml'});
-  res.end(twiml.toString());
+  // res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.send("Message received!");
+  // res.json({
+  //   message: req.body.Body
+  // });
 });
 
 // catch 404 and forward to error handler
@@ -38,7 +42,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');//this or res.status(err.status || 500).send('error')
 });
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 module.exports = app;
